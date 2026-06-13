bf82b1b7bcf58d93ba63c722f754b0f6cc2f2991
palsurya24 <palsurya2414@gmail.com>
2026-06-13 19:38:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 });
 
 // Routes
-//app.use('/api/auth', require('./routes/auth'));
+app.use('/api/auth', require('./routes/auth'));
 //app.use('/api/transactions', require('./routes/transactions'));
 //app.use('/api/friends', require('./routes/friends'));
 //app.use('/api/splits', require('./routes/splits'));
